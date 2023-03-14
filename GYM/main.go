fe5766d609cbbc83f81a68efb5bf223269063a2a
OnkarOjha <1908288@sbsstc.ac.in>
2023-03-14 18:36:27 +0530

    Added 5 more routes with restructuring DB IN GYM Task

--- GYM/main.go
+++ GYM/main.go
@@ -1,19 +1,18 @@
 package main
 
-
 import (
 	"encoding/json"
 	"fmt"
 	"log"
+	models "main/models"
 	"net/http"
 	"sort"
 	"strconv"
 	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	models "main/models"
 )
-
 
 var DB *gorm.DB
 
@@ -21,6 +20,220 @@ var DB *gorm.DB
 var memberships []models.MemberInfo = []models.MemberInfo{
 	{Id: "1", Name: "Onkar", Membership: "Gold", StartDate: "2023-01-01", EndDate: "2023-03-01", MoneySubmitted: 12000, MonthlyPrice: 2000, Duration: 4, IsDeleted: false},
 	{Id: " 2", Name: "Aman", Membership: "Silver", StartDate: "2023-02-01", EndDate: "2023-05-01", MoneySubmitted: 6000, MonthlyPrice: 1000, Duration: 5},
+}
+
+func UserEnrollment(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data!!!")
+
+	}
+	var user models.Users
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	result := DB.Create(&user)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "User Enrollment successful")
+
+}
+func SubscriptionHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("You will be entering subscription details for your user")
+	fmt.Println("Please enter your id in params")
+
+	params := r.URL.Query().Get("id")
+	fmt.Println("params: ", params)
+
+	var subscription models.Subscription
+	_ = json.NewDecoder(r.Body).Decode(&subscription)
+	// ? user_id set from params
+
+	subscription.User_id = params
+
+	//? payment id from payment var
+
+	// TODO employee id from employee var
+	// var employee models.GymEmployee
+	// fmt.Println("employee: ", employee.Employee_id)
+	// subscription.Employee_id = employee.Employee_id
+
+	// ? started_date from time.now()
+	now := time.Now().Truncate(24 * time.Hour)
+	subscription.Start_date = now.Format("2006-01-02")
+	startTime, err := time.Parse("2006-01-02", subscription.Start_date)
+	if err != nil {
+		fmt.Println("Error parsing start date:", err)
+		return
+	}
+	// ? end date set
+	subscription.End_date = startTime.AddDate(0, int(subscription.Duration), 0).Format("2006-01-02")
+
+	//? end date will be deletedAt
+
+	// ?  subscription type will be according to the price table values
+
+	result := DB.Create(&subscription)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "User Subscription successful")
+}
+
+func PaymentHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query().Get("id")
+	fmt.Println("params: ", params)
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data!!!")
+
+	}
+
+	var payment models.Payment
+	_ = json.NewDecoder(r.Body).Decode(&payment)
+
+	//? date handler
+	now := time.Now().Truncate(24 * time.Hour)
+	payment.Date = now
+
+	// ? amount apne aap calculate krega
+	var subscription models.Subscription
+
+	// ? paymen hone k baad update
+
+	DB.Where("user_id =?", params).First(&subscription)
+
+	payment.User_id = params
+	// subscription.Payment_id = payment.Payment_id
+	fmt.Println("Subscription: ", subscription.Duration)
+	var price models.Price
+	if subscription.Membership_type == "Gold" {
+		DB.Where("name=?", "Gold").First(&price)
+		payment.Amount = subscription.Duration * price.Price
+	} else if subscription.Membership_type == "Silver" {
+		DB.Where("name=?", "Silver").First(&price)
+		payment.Amount = subscription.Duration * price.Price
+	}
+
+	result := DB.Create(&payment)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	subscription.Payment_id = payment.Payment_id
+	DB.Where("user_id =?", params).Updates(&subscription)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "Payment Done")
+
+}
+
+func EndSubscriptionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println("You are going to end your membership!!")
+	fmt.Println("Please enter your Id in params")
+
+	params := r.URL.Query().Get("id")
+	fmt.Println("params: ", params)
+	var subscription models.Subscription
+	now := time.Now().Truncate(24 * time.Hour)
+	DB.Where("user_id =?", params).First(&subscription)
+
+	if subscription.User_id == params{
+	
+		// end_date := now.Format("2006-01-02")
+		var payment models.Payment
+		var amount float64
+		DB.Where("user_id =?", params).Find(&payment)
+		if payment.User_id == params{
+			amount = payment.Amount
+			fmt.Println("amount" ,amount)
+		}
+		
+		// refundedMoney := payment.Amount / 2
+		fmt.Println("dmknfgskf")
+		DB.Where("user_id =?", params).Updates(&models.Subscription{IsDeleted: true ,End_date:now.Format("2006-01-02"),RefundedMoney:  amount / 2 })
+		// DB.Where("user_id=?",params).Delete(&subscription)
+
+		json.NewEncoder(w).Encode(&subscription)
+	}
+	
+	fmt.Fprintf(w, "Users Subscription is now ended")
+
+}
+
+func EmployeeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data!!!")
+
+	}
+
+	var employee models.GymEmployee
+	_ = json.NewDecoder(r.Body).Decode(&employee)
+
+	var subscription models.Subscription
+
+	result := DB.Create(&employee)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	//? user id ei params mei pass kr rha haui
+	params := r.URL.Query().Get("id")
+	fmt.Println("params: ", params)
+	subscription.Employee_id = employee.Employee_id
+	subscription.Trainer_name = employee.First_name
+	DB.Where("user_id =?", params).Updates(&subscription)
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "Employee Enrollment successful")
+
+}
+
+func EquipementHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("You are adding a new equipement")
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data!!!")
+
+	}
+
+	var equipment models.Equipment
+	_ = json.NewDecoder(r.Body).Decode(&equipment)
+
+	result := DB.Create(&equipment)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "Equipment Enrolled successful")
+
+}
+
+func ShowUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var show []models.Subscription
+	DB.Joins("User").Find(&show)
+	json.NewEncoder(w).Encode(&show)
 }
 
 func EnrollHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,7 +246,7 @@ func EnrollHandler(w http.ResponseWriter, r *http.Request) {
 
 	}
 	var price models.Price
-	var member  models.MemberInfo
+	var member models.MemberInfo
 	_ = json.NewDecoder(r.Body).Decode(&member)
 
 	//^ membership check
@@ -96,6 +309,7 @@ func EnrollHandler(w http.ResponseWriter, r *http.Request) {
 	memberships = append(memberships, member)
 
 	// Handle enrollment logic here
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, "Enrollment successful")
 
 }
@@ -114,8 +328,8 @@ func ShowMembersHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Reading users failed", http.StatusNotFound)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(memberships) // this will send response as a json value
-
 	fmt.Fprintf(w, "Reading successful")
 }
 
@@ -155,8 +369,8 @@ func EndMembershipHandler(w http.ResponseWriter, r *http.Request) {
 			memberships = append(memberships, member)
 			memberships = append(memberships[:i], memberships[i+1:]...)
 
-			DB.Where("id =?", params).Updates(& models.MemberInfo{RefundedMoney: MoneyRefund / 2, Duration: float64(days / 30), EndDate: now.Format("2006-01-02"), IsDeleted: true})
-			DB.Where("id=?", params).Delete(&member)
+			DB.Where("id =?", params).Updates(&models.MemberInfo{RefundedMoney: MoneyRefund / 2, Duration: float64(days / 30), EndDate: now.Format("2006-01-02"), IsDeleted: true})
+			DB.Where("id=?", params).Delete(&member) //  actual deleting
 
 			json.NewEncoder(w).Encode(member)
 
@@ -169,20 +383,21 @@ func EndMembershipHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func MembershipPriceShowHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hy Welcome here are our Membership Prices")
+	// fmt.Fprintln(w, "Hy Welcome here are our Membership Prices")
 	var memPrice []models.Price
 	result := DB.Find(&memPrice)
 	if result.Error != nil {
 		http.Error(w, "Reading users failed", http.StatusNotFound)
 		return
 	}
-	json.NewEncoder(w).Encode(memPrice)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&memPrice)
 }
 
 func MembershipPriceUpdateHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Now you can update membership prices!!")
 
-	var memPrice  models.Price
+	var memPrice models.Price
 	err := json.NewDecoder(r.Body).Decode(&memPrice)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -196,7 +411,6 @@ func MembershipPriceUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
 func main() {
 	fmt.Println("Implementing GYM membership Task...")
 
@@ -207,10 +421,17 @@ func main() {
 		panic("error connecting to the database")
 	}
 
-	db.AutoMigrate(&models.MemberInfo{}, &models.Price{})
+	db.AutoMigrate(&models.MemberInfo{}, &models.Price{}, &models.Users{}, &models.Subscription{}, &models.Payment{}, &models.Equipment{}, &models.GymEmployee{})
 	DB = db
 
 	r := http.NewServeMux()
+	r.HandleFunc("/user", UserEnrollment)
+	r.HandleFunc("/subscription", SubscriptionHandler)
+	r.HandleFunc("/endSubs", EndSubscriptionHandler)
+	r.HandleFunc("/employee", EmployeeHandler)
+	r.HandleFunc("/payment", PaymentHandler)
+	r.HandleFunc("/equipment", EquipementHandler)
+	r.HandleFunc("/show", ShowUsersHandler)
 	r.HandleFunc("/enroll", EnrollHandler)
 	r.HandleFunc("/members", ShowMembersHandler)
 	r.HandleFunc("/end", EndMembershipHandler)
--- GYM/models/models.go
+++ GYM/models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -22,3 +24,59 @@ type Price struct {
 	Name  string
 	Price float64
 }
+
+// users struct
+type Users struct {
+	User_id        string `json:"user_id" gorm:"default:uuid_generate_v4();uniqueIndex"`
+	First_name     string `json:"first_name"`
+	Last_name      string `json:"last_name"`
+	Gender         string `json:"gender" `
+	Contact_Number string `json:"contact_number"`
+}
+
+// subscriptions struct
+type Subscription struct {
+	Payment_id string `json:"payment_id"`
+	// Payment           Payment     `gorm:"references:Payment_id"` //FK
+	User_id         string  `json:"user_id"`
+	User            Users   `gorm:"references:User_id"` //FK
+	Start_date      string  `json:"start_date"`
+	Membership_type string  `json:"membership_type"`
+	End_date        string  `json:"end_date"`
+	Duration        float64 `json:"duration"`
+	Employee_id     string  `json:"employee_id"`
+	// Employee          GymEmployee `gorm:"references:Employee_id"` //FK
+	Trainer_name  string  `json:"trainer_name"`
+	IsDeleted     bool    `json:"isDeleted"`
+	RefundedMoney float64 `json:"refundedMoney"`
+}
+
+// Payment struct
+type Payment struct {
+	Payment_id   string    `json:"payment_id" gorm:"default:uuid_generate_v4();uniqueIndex"`
+	User_id      string    `json:"user_id"`
+	User         Users     `gorm:"references:User_id"` //FK
+	Amount       float64   `json:"amount"`
+	Date         time.Time `json:"date"`
+	Payment_type string    `json:"payment_type"`
+}
+
+// Equipment struct
+type Equipment struct {
+	Model_number string `json:"model_number" gorm:"default:uuid_generate_v4();unique"`
+	Equip_name   string `json:"equip_name"`
+	Quantity     int64  `json:"quantity"`
+}
+
+type GymEmployee struct {
+	Employee_id    string `json:"employee_id" gorm:"default:uuid_generate_v4();unique"`
+	First_name     string `json:"first_name"`
+	Last_name      string `json:"last_name"`
+	Gender         string `json:"gender"`
+	Contact_Number string `json:"contact_number"`
+	Role           string `json:"role"`
+}
+
+// TODO user attendance
+// TODO Employee attendance
+// TODO offers
